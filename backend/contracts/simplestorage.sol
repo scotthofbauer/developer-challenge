f8875073131297bdf8f1e76b9e4f0282ad233503
Scott Hofbauer <glynn.hofbauer@levvel.io>
2021-12-06 11:58:47 -0500

    initial setup working

--- backend/contracts/simplestorage.sol
+++ backend/contracts/simplestorage.sol
@@ -1,27 +1,31 @@
-pragma solidity >=0.4.24 <0.6.0;
+pragma solidity >=0.4.24;
+
 /**
-  * @title Simple Storage
-  * @dev Read and write values to the chain
-  */
+ * @title Simple Storage
+ * @dev Read and write values to the chain
+ */
 contract simplestorage {
     string public _x;
+
     /**
-      * @dev Constructor sets the default value
-      * @param x The initial value
-      */
+     * @dev Constructor sets the default value
+     * @param x The initial value
+     */
     constructor(string memory x) public {
         _x = x;
     }
+
     /**
-      * @dev Set the value
-      * @param x The new value
-      */
+     * @dev Set the value
+     * @param x The new value
+     */
     function set(string memory x) public {
         _x = x;
     }
+
     /**
-      * @dev Get the value
-      */
+     * @dev Get the value
+     */
     function get() public view returns (string memory x) {
         return _x;
     }
