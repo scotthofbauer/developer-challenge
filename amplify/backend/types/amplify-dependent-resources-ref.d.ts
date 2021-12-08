21a1a7bf6807806e71dc7a2bca5885974b2ad6b9
Scott Hofbauer <glynn.hofbauer@levvel.io>
2021-12-07 21:57:31 -0500

    initial amplify setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "developerchallenge": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Amplify from 'aws-amplify';
 import React from 'react';
+import awsExports from './aws-exports';
 import Home from './pages/home/Home';
-require('dotenv').config()
 // import { Route, Router, Routes } from 'react-router-dom';
+Amplify.configure(awsExports);
 
 
 const App = () => {
