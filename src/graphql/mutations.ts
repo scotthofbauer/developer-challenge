a0938c61877ef8e99f334273a32b99c16584f145
Scott Hofbauer <glynn.hofbauer@levvel.io>
2021-12-07 23:46:33 -0500

    updated schema for contracts and tokens

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,184 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createContract = /* GraphQL */ `
+  mutation CreateContract(
+    $input: CreateContractInput!
+    $condition: ModelContractConditionInput
+  ) {
+    createContract(input: $input, condition: $condition) {
+      id
+      title
+      gatewayAPIId
+      isERCBurnable
+      isERCMintable
+      isERC721
+      isERC20
+      creator
+      contractName
+      tokens {
+        items {
+          id
+          content
+          createdAt
+          updatedAt
+          contractTokensId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateContract = /* GraphQL */ `
+  mutation UpdateContract(
+    $input: UpdateContractInput!
+    $condition: ModelContractConditionInput
+  ) {
+    updateContract(input: $input, condition: $condition) {
+      id
+      title
+      gatewayAPIId
+      isERCBurnable
+      isERCMintable
+      isERC721
+      isERC20
+      creator
+      contractName
+      tokens {
+        items {
+          id
+          content
+          createdAt
+          updatedAt
+          contractTokensId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteContract = /* GraphQL */ `
+  mutation DeleteContract(
+    $input: DeleteContractInput!
+    $condition: ModelContractConditionInput
+  ) {
+    deleteContract(input: $input, condition: $condition) {
+      id
+      title
+      gatewayAPIId
+      isERCBurnable
+      isERCMintable
+      isERC721
+      isERC20
+      creator
+      contractName
+      tokens {
+        items {
+          id
+          content
+          createdAt
+          updatedAt
+          contractTokensId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createToken = /* GraphQL */ `
+  mutation CreateToken(
+    $input: CreateTokenInput!
+    $condition: ModelTokenConditionInput
+  ) {
+    createToken(input: $input, condition: $condition) {
+      id
+      contract {
+        id
+        title
+        gatewayAPIId
+        isERCBurnable
+        isERCMintable
+        isERC721
+        isERC20
+        creator
+        contractName
+        tokens {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      content
+      createdAt
+      updatedAt
+      contractTokensId
+    }
+  }
+`;
+export const updateToken = /* GraphQL */ `
+  mutation UpdateToken(
+    $input: UpdateTokenInput!
+    $condition: ModelTokenConditionInput
+  ) {
+    updateToken(input: $input, condition: $condition) {
+      id
+      contract {
+        id
+        title
+        gatewayAPIId
+        isERCBurnable
+        isERCMintable
+        isERC721
+        isERC20
+        creator
+        contractName
+        tokens {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      content
+      createdAt
+      updatedAt
+      contractTokensId
+    }
+  }
+`;
+export const deleteToken = /* GraphQL */ `
+  mutation DeleteToken(
+    $input: DeleteTokenInput!
+    $condition: ModelTokenConditionInput
+  ) {
+    deleteToken(input: $input, condition: $condition) {
+      id
+      contract {
+        id
+        title
+        gatewayAPIId
+        isERCBurnable
+        isERCMintable
+        isERC721
+        isERC20
+        creator
+        contractName
+        tokens {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      content
+      createdAt
+      updatedAt
+      contractTokensId
+    }
+  }
+`;
